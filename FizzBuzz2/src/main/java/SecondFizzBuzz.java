5ffd9591e146542fbf8b3eb76d0b27da41dab1e0
evanrlay <evanrlay@gmail.com>
2020-04-20 13:59:55 -0600

    Add starting range

--- FizzBuzz2/src/main/java/SecondFizzBuzz.java
+++ FizzBuzz2/src/main/java/SecondFizzBuzz.java
@@ -1,13 +1,20 @@
 public class SecondFizzBuzz {
     public static void main(String[] args)
     {
-        System.out.println(getSolutionForNumber(20));
+        System.out.println(getSolutionForNumber(1, 20));
+        System.out.println(getSolutionForNumber(10, 5));
+        System.out.println(getSolutionForNumber(1, 1));
+        System.out.println(getSolutionForNumber(-10, 10));
     }
 
-    public static String getSolutionForNumber(int targetNumber) {
+    public static String getSolutionForNumber(int start, int end) {
+        if(start > end) {
+            return "Starting index cannot be larger than ending index";
+        }
+
         StringBuilder output = new StringBuilder();
 
-        for(int i = 1; i <= targetNumber; i++){
+        for(int i = start; i <= end; i++){
             String number = String.valueOf(i);
             if(number.contains("3")) {
                 output.append("lucky ");
--- FizzBuzz2/src/test/java/SecondFizzBuzzTest.java
+++ FizzBuzz2/src/test/java/SecondFizzBuzzTest.java
@@ -2,9 +2,33 @@ import org.junit.Test;
 
 public class SecondFizzBuzzTest {
     @Test
-    public void testFizzBuzz() {
+    public void testFizzBuzzBadRange() {
+        String expectedAnswer = "Starting index cannot be larger than ending index";
+        String answer = SecondFizzBuzz.getSolutionForNumber(20, 10);
+
+        assert(answer.equals(expectedAnswer));
+    }
+
+    @Test
+    public void testFizzBuzzNegativeNumbers() {
+        String expectedAnswer = "buzz fizz -8 -7 fizz buzz -4 lucky -2 -1 fizzbuzz 1 2 lucky 4 buzz fizz 7 8 fizz buzz ";
+        String answer = SecondFizzBuzz.getSolutionForNumber(-10, 10);
+
+        assert(answer.equals(expectedAnswer));
+    }
+
+    @Test
+    public void testFizzBuzz0Range() {
+        String expectedAnswer = "1 ";
+        String answer = SecondFizzBuzz.getSolutionForNumber(1, 1);
+
+        assert(answer.equals(expectedAnswer));
+    }
+
+    @Test
+    public void testFizzBuzz20() {
         String expectedAnswer = "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz ";
-        String answer = SecondFizzBuzz.getSolutionForNumber(20);
+        String answer = SecondFizzBuzz.getSolutionForNumber(1, 20);
 
         assert(answer.equals(expectedAnswer));
 
